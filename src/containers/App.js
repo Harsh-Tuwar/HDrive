67fda725c806d7f48a53900ca1540ee0840c0016
Harsh Tuwar <tuwarharsh08@gmail.com>
2021-02-06 20:57:11 -0400

    Test 2

--- src/containers/App.js
+++ src/containers/App.js
@@ -23,6 +23,8 @@ const App = () => {
 		test();
 	}, []);
 	
+	console.log(process.env.REACT_APP_API_KEY_MASTER);
+	
 	document.addEventListener("contextmenu", (e) => e.preventDefault());
 	return(
 		<Provider store={store}>
