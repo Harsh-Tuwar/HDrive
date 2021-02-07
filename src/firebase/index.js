729dbaf408b7c157c23c0217cb2b0d930afbc093
Harsh Tuwar <tuwarharsh08@gmail.com>
2021-02-06 23:07:25 -0400

    dep updated

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -6,12 +6,12 @@ import "firebase/storage";
 
 // Master Project
 const firebaseConfigMaster = {
-	apiKey: process.env.REACT_APP_API_KEY_MASTER,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN_MASTER,
-	projectId: process.env.REACT_APP_PROJECT_ID_MASTER,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET_MASTER,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_MASTER,
-	appId: process.env.REACT_APP_APP_ID_MASTER
+	apiKey: process.env.REACT_APP_API_KEY_MAIN,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN_MAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID_MAIN,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET_MAIN,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_MAIN,
+	appId: process.env.REACT_APP_APP_ID_MAIN
 };
 
 const masterApp = firebase.initializeApp(firebaseConfigMaster, "masterApp");
