5001bd4ae31baaba465b1d0b2f3e887f4b9a1bc2
Harsh Tuwar <tuwarharsh08@gmail.com>
2021-03-18 22:56:40 -0300

    updates for redux devtools

--- src/redux/store/config.js
+++ src/redux/store/config.js
@@ -33,7 +33,7 @@ const config = (preloadState) => {
 		preloadState,
 		compose(
 			applyMiddleware(...middlewares),
-			(process.env.NODE_ENV === "development") ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
+			// (process.env.NODE_ENV === "development") ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
 		)
 	);
 };
